3ea3f02bceb55471b7bda0c6ff088ca1621756b9
Vector2588 <natkamol082@gmail.com>
2021-07-21 21:15:21 +0700

    2.9 show more card

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ const routes = [
   {
     path: '/',
     name: 'EventList',
-    component: EventList
+    component: EventList,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 , perPage: parseInt(route.query.perPage) || 1})
   },
   {
     path: '/about',
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,30 @@
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { perPage,page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1">
+        Prev Page
+      </router-link>
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { perPage,page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage">
+        Next Page
+      </router-link>
+    </div>
+    <div class="showcard">
+      <router-link id="del-card" :to="{ name: 'EventList', query: { perPage: perPage - 1,page }}" >
+        Show less card
+      </router-link>
+      <router-link id="add-card" :to="{ name: 'EventList', query: { perPage: perPage + 1,page } }" >
+        Show more card
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -9,25 +33,47 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 // import axios from 'axios'
 export default {
   name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    perPage: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard // register it as a child component
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0,
+      totalCard: 0,
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(this.perPage, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / this.perPage)
+      return this.page < totalPages
+    }
   }
 }
 </script>
@@ -36,5 +82,39 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
+}
+
+.showcard {
+  display: flex;
+  width: 290px;
+}
+
+.showcard a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#del-card {
+  text-align: left;
+}
+#add-card {
+  text-align: right;
 }
 </style>
